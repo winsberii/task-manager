a9805997df9ce513cac85c1875b52c736984ef0e
winsberii <wins.berii@gmail.com>
2025-08-14 07:23:48 +0300

    Fix dependency and import resolution errors

--- vite.config.ts
+++ vite.config.ts
@@ -9,6 +9,9 @@ export default defineConfig(({ mode }) => ({
     host: "::",
     port: 8080,
   },
+  optimizeDeps: {
+    exclude: ['@node-rs/argon2-wasm32-wasi']
+  },
   plugins: [
     react(),
     mode === 'development' &&
